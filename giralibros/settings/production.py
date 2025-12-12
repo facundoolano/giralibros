1592d051096b4144941b6cc2d01163a3e6fe02c5
facundo <facundo.olano@gmail.com>
2025-12-12 18:43:12 -0300

    another try

--- giralibros/settings/production.py
+++ giralibros/settings/production.py
@@ -19,7 +19,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        "BACKEND": "giralibros.storage.ForgivingManifestStaticFilesStorage",
     },
 }
 
